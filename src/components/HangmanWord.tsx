9daac98fd9ce55aa5385ab5db951a2c992d36532
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-11-26 13:53:07 +0530

    add logic for HangmanWord.tsx

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,5 +1,40 @@
 const HangmanWord = () => {
-  return <div>HangmanWord</div>;
+  const word = 'test';
+  const guessedLetters = ['t', 'e'];
+  return (
+    <div
+      className="container"
+      style={{
+        display: 'flex',
+        gap: '.25em',
+        fontSize: '5rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace',
+      }}
+    >
+      {word.split('').map((letter, index) => {
+        return (
+          <span
+            style={{
+              borderBottom: '.1em solid black',
+            }}
+            key={index}
+          >
+            <span
+              style={{
+                visibility: guessedLetters.includes(letter)
+                  ? 'visible'
+                  : 'hidden',
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
 };
 
 export default HangmanWord;
